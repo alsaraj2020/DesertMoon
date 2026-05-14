c757d131398e2047050a11e88f6459f1739393bc
alsaraj2020 <94948197+alsaraj2020@users.noreply.github.com>
2026-05-14 12:08:35 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
+  build: {
+    target: "esnext"
   },
   optimizeDeps: {
-    include: ["buffer", "process"],
-  },
-  resolve: {
-    alias: {
-      buffer: "buffer",
-      process: "process/browser",
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+    esbuildOptions: {
+      target: "esnext"
+    }
+  }
 });
